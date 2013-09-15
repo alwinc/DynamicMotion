32bbdbe3f79cc87056d6ba772f1d2b6da6a6572f
ashussen <albertsamuelhussen@gmail.com>
2013-09-15 22:47:12 +1000

    try a little changes

--- Prototype/login.php
+++ Prototype/login.php
@@ -1,3 +1,6 @@
+<?php
+	session_start();	
+?>
 <!doctype html>
 <html>
 <head>
@@ -27,35 +30,43 @@ if (/*@cc_on!@*/false) {
   <!-- end .accountbar -->
   <div class="header-wrap">
     <ul class="group" id="header-one" name="header-one">
-      <li> <a href="#">Home</a></li>
-      <li class="current_page_item"><a href="#">Login</a></li>
-      <li><a href="#">Register</a></li>
-      <li><a href="#">MyAccount</a></li>
-      <li><a href="#">Pest Sight</a></li>
-      <li><a href="#">Weekly Report</a></li>
-      <li><a href="#">Pest Register</a></li>
+      <li> <a href="index.html">Home</a></li>
+      <li class="current_page_item" name="login_loginpage"><a href="login.html">Login</a></li>
+      <li name="login_Registerpage"><a href="register.html">Register</a></li>
+      <li name="login_Userpage"><a href="#">MyAccount</a></li>
+      <li name="login_Pest Fight"><a href="#">Pest Fight</a></li>
+      <li name="login_Reportpage"><a href="#">Weekly Report</a></li>
+      <li name="login_P_Registerpage"><a href="#">Pest Register</a></li>
     </ul>
   </div>
   <!-- end .header-warp -->
   <div class="login">
     <ul id="loginform-bg" name="loginform-bg">
       <section class="loginform cf">
-        <form name="login" action="index_submit" method="get" accept-charset="utf-8">
+        <form name="login" action="checklogin.php" method="post" accept-charset="utf-8">
           <ul>
             <li><img src="image/loginpage-02.png"></li>
             <li class="error" id="issue-massage"name="issue-massagee">
-              <p>*You have issue message:</p>
-              <p>Hello World!</p>
+              <p style="color:red;">
+					<?php	
+						//display the error msg if the login credentials are wrong!
+						if(isset($_SESSION['login_msg']))
+						{
+							echo $_SESSION['login_msg'];
+							unset($_SESSION['login_msg']);
+						}
+					?>
+				</p>
             </li>
             <li>
               <label for="usermail" class="user_mail">E-Mail Address</label>
-              <input type="email" name="login_usermail" placeholder="Enter E-mail Address" required>
+              <input type="email" required="required" name="login_usermail" placeholder="Enter E-mail Address" required>
             </li>
             <li>
               <label for="password" class="password">Password</label>
-              <input type="password" name="login_password" placeholder="Enter Password" required>
+              <input type="password" required="required" name="login_password" placeholder="Enter Password" required>
             </li>
-            <li class="login_button" name="login_button"> <a href="">Log in</a> </li>
+            <li class="login_button" name="login_button"> <a href="checklogin.php" onclick="document.forms['login'].submit(); return false;">Log in</a> </li>
             <li>
               <input type="checkbox" name="checkboxG1" value="checkbox" id="checkboxG1">
               <label class="checkbox">Keep me logged in</label>
@@ -64,7 +75,7 @@ if (/*@cc_on!@*/false) {
           <ul class="support-bg"id="support-qa" name="support-qa">
             <li>
               <h5>Can't <a href="">log in </a>?</h5>
-              <h5>Learn how to <a href="">protent your account</a>!</h5>
+              <h5>Learn how to <a href="" onclick="document.forms[0].submit();return false;">protent your account</a>!</h5>
               <h5>Don't have an account yet?<a href=""> Sign up now </a>! </h5>
             </li>
           </ul>
