93727781374b83f057da9c36ca89287d47033c44
andrewindayang <andrewindayang@gmail.com>
2013-10-22 15:49:26 +1100

    Update checklogin.php

--- Prototype/checklogin.php
+++ Prototype/checklogin.php
@@ -18,6 +18,8 @@
 	//query for checking usermail and pass
 	$query = "SELECT name FROM person WHERE Email='$currentusermail' and password = '$currentpassword'";
 	$result = mysqli_query($link,$query);
+	$row = mysqli_fetch_assoc($result);
+	$name = $row['name'];
 	
 	//count the row number of the query
 	$count = mysqli_num_rows($result);
@@ -25,15 +27,12 @@
 	//check if the login match and do action
 	if($count == 1)
 	{
-		//$_SESSION['$currentusermail'] = $_POST['$currentusermail'];		
-		//$result = mysqli_real_escape_string($link,$result);	
-		//$try = urlencode($result);	
-		//$_SESSION['login_user'] = $result;
-		header("location: index.php?username="."$currentusermail");
+		header("location: home.php?username="."$name");
 	}
 	else
 	{
-		$_SESSION['login_msg'] ="Wrong username or Password!";
+		$_SESSION['login_msg'] = 1;
+		$error = "Wrong Username or Password";
 		header("location:login.php");  
 	}
 ?>
