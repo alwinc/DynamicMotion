1e11cac517faf08de6807251e9999aefb0379a72
ashussen <albertsamuelhussen@gmail.com>
2013-10-28 18:02:30 +1100

    pestSighting update and delete

--- Prototype/old layout/deletePestSighting.php
+++ Prototype/old layout/deletePestSighting.php
@@ -8,11 +8,9 @@ mysqli_select_db($link,"pesttracking");
 
 //write code for update button to update pest information
 
-if(isset($_POST['update']))
+if(isset($_POST['delete']))
 {
-	$UpdateQuery = "UPDATE sighting SET sightingId='$_POST[sightingId]', parkId='$_POST[parkId]', time='$_POST[time]', 
-	 latlng='$_POST[latlng]', pestDetails='$_POST[pestDetails]',
-	 pestId='$_POST[pestId]', numberOfPestsSighted='$_POST[numberOfPestsSighted]' WHERE `sightingId`='$_POST[sightingId]' ";
+	$UpdateQuery = "DELETE FROM sighting WHERE sightingId= '$_POST[sightingId]'";
 	mysqli_query($link,$UpdateQuery);
 };
 
@@ -35,7 +33,7 @@ echo "<table border=1>
 //update pest information
 while($record = mysqli_fetch_array($pestData))
 {
-echo "<form action=pestSightingupdate.php method=post>";
+echo "<form action=deletePestSighting.php method=post>";
 echo "<tr>";
 echo "<td>" . "<input type=text size=7 name=sightingId value=" . $record['sightingId'] . " style=border:0px readonly></td>";
 echo "<td>" . "<input type=text size=4 name=pestId value=" . $record['pestId'] . " style=border:0px></td>";
@@ -44,7 +42,7 @@ echo "<td>" . "<input type=text size=17 name=numberOfPestsSighted value=\"" . $r
 echo "<td>" . "<input type=text size=15 name=time value=" . $record['time'] . " style=border:0px></td>";
 echo "<td>" . "<input type=text name=latlng value=\"" . $record['latlng'] . "\" style=border:0px></td>";
 echo "<td>" . "<input type=text name=pestDetails value=\"" . $record['pestDetails'] . "\" style=border:0px></td>";
-echo "<td>" . "<input type=submit name=update value=update></td>";
+echo "<td>" . "<input type=submit name=delete value=delete></td>";
 echo "</tr>";
 echo "</form>";
 
