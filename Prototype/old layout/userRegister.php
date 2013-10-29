37af7da0570709156181bf5e4db94ac6a24c15e3
victortanXXX <th527501@gmail.com>
2013-10-29 17:09:31 +1100

    Update userRegister.php

--- Prototype/old layout/userRegister.php
+++ Prototype/old layout/userRegister.php
@@ -30,7 +30,7 @@ if (/*@cc_on!@*/false) {
       <ul class="accountbar-bg" id="account-bar" name="account-bar">
         <li class="support-hm"><a href="index.html">Home</a></li>
         <li class="support-lg"><a href="login.php">Login</a> or <a href="userRegister.php">Register</a></li>
-        <li class="support-ac"><a href="#">MyAccount</a></li>
+        <li class="support-ac"><a href="myAccount.php">MyAccount</a></li>
         <li class="support-sp"><a href="#">Support</a></li>
       </ul>
     </div>
@@ -38,13 +38,13 @@ if (/*@cc_on!@*/false) {
   <!-- end .accountbar -->
   <div class="header-wrap">
     <ul class="group" id="header-one" name="header-one">
-      <li id="home-wrap"> <a href="#">Home</a></li>
+      <li id="home-wrap"> <a href="home.php">Home</a></li>
       <li id="login-wrap"><a href="login.php">Login</a></li>
       <li id="user-wrap" class="current_page_item"><a href="userRegister.php">Register</a></li>
-      <li id="account-wrap"><a href="login.php">MyAccount</a></li>
-      <li id="sighing-wrap"><a href="login.php">Pest Sight</a></li>
-      <li id="report-wrap"><a href="login.php">Weekly Report</a></li>
-      <li id="register-wrap"><a href="login.php">Pest Register</a></li>
+      <li id="account-wrap"><a href="myAccount.php">MyAccount</a></li>
+      <li id="sighing-wrap"><a href="pestSighting.php">Pest Sight</a></li>
+      <li id="report-wrap"><a href="#">Weekly Report</a></li>
+      <li id="register-wrap"><a href="pestRegister.php">Pest Register</a></li>
     </ul>
   </div>
   <!-- end .header-warp -->
