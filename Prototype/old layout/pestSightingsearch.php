6ba0d03ece5dce0f98083404e4cd89dbd4d1436f
victortanXXX <th527501@gmail.com>
2013-10-29 21:29:18 +1100

    Update pestSightingsearch.php

--- Prototype/old layout/pestSightingsearch.php
+++ Prototype/old layout/pestSightingsearch.php
@@ -32,7 +32,7 @@ if (/*@cc_on!@*/false) {
       <ul class="accountbar-bg" id="account-bar" name="account-bar">
         
         <!-- Check if already login then write welcome-->
-        <li class="support-ac"><a href="myAccount">MyAccount</a></li>
+        <li class="support-ac"><a href="myAccount.php">MyAccount</a></li>
         <li class="support-sp"><a href="#">Support</a></li>
         <li class="support-lo"><a href="logout.php">Log off</a></li>
         <li class ="php-user">
@@ -58,7 +58,7 @@ if (/*@cc_on!@*/false) {
       <li id="account-wrap"><a href="myAccount.php">MyAccount</a></li>
       <li id="sighing-wrap" class="current_page_item"><a href="pestSightingIndex.php">Pest Sight</a></li>
       <li id="report-wrap"><a href="managementReport.php">Weekly Report</a></li>
-      <li id="register-wrap"><a href="pestRegister.html">Pest Register</a></li>
+      <li id="register-wrap"><a href="pestRegister.php">Pest Register</a></li>
     </ul>
   </div>
   <!-- end .header-warp -->
