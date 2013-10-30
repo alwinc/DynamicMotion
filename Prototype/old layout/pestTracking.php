ee7d87bb7981262984847a51595ff99e958fe0dd
andrewindayang <andrewindayang@gmail.com>
2013-10-30 15:44:20 +1100

    Update pestTracking.php
    update the pest tracking page

--- Prototype/old layout/pestTracking.php
+++ Prototype/old layout/pestTracking.php
@@ -1,6 +1,6 @@
-﻿<!doctype html>
+<!doctype html>
 <html>
-<head><link rel="icon" href="image/favicon.ico">
+<head>
 <meta charset="utf-8">
 <title>Animals Map</title>
 -<link href="css/styles.css" rel="stylesheet" type="text/css">
@@ -27,14 +27,14 @@ if (/*@cc_on!@*/false) {
       <ul class="accountbar-bg" id="account-bar" name="account-bar">
         
         <!-- Check if already login then write welcome-->
-        <li class="support-ac"><a href="myAccount.php">MyAccount</a></li>
+        <li class="support-ac"><a href="#">MyAccount</a></li>
         <li class="support-sp"><a href="#">Support</a></li>
-        <li class="support-lo"><a href="logout.php">Log off</a></li>
+        <li class="support-lo"><a href="index.html">Log off</a></li>
         <li class ="php-user">
           <?php
-			if(isset($_SESSION['username']))
+			if(isset($_GET['username']))
 			{
-				echo "Welcome, ". $_SESSION['username'];
+				echo "Welcome, ". $_GET['username'];
 			}
 		?>
         </li>
@@ -47,13 +47,13 @@ if (/*@cc_on!@*/false) {
   <!-- end .accountbar -->
   <div class="header-wrap">
     <ul class="group" id="header-one" name="header-one">
-      <li id="home-wrap"> <a href="home.php">Home</a></li>
+      <li id="home-wrap" class="current_page_item"> <a href="#">Home</a></li>
       <li id="login-wrap"><a href="login.php">Login</a></li>
       <li id="user-wrap"><a href="userRegister.php">Register</a></li>
-      <li id="account-wrap"><a href="myAccount.php">MyAccount</a></li>
+      <li id="account-wrap"><a href="#">MyAccount</a></li>
       <li id="sighing-wrap"><a href="pestSightingIndex.php">Pest Sight</a></li>
-      <li id="report-wrap"><a href="managementReport">Weekly Report</a></li>
-      <li id="register-wrap" class="current_page_item"><a href="pestTracking.php">Pest Tracking</a></li>
+      <li id="report-wrap"><a href="#">Weekly Report</a></li>
+      <li id="register-wrap"><a href="pestRegister.php">Pest Register</a></li>
     </ul>
   </div>
   <!-- end .header-warp -->
@@ -63,13 +63,18 @@ if (/*@cc_on!@*/false) {
         <form name="pest_tracking" action="pestTracking.php" method="post" accept-charset="utf-8">
           <ul>
             <li><img src="image/loginpage-02.png"></li>
-            <li id = "search_bar">
+			
+			<!--textbox and submit button-->
+			            <li id = "search_bar">
               <label for="pest_search" class="pest_search">Search by Pest Id</label>
-              <input id = "search_input" name="search_input" required type="text" placeholder="Please input Pest Id" />
+              <input id = "search_input" name="search_input" required type="text" placeholder="Pest Id" />
             </li>
+			
+			<!--Button Submit-->
             <li class="submit-search"> <a href="#">
               <input type="submit" id="submit_button_search" name="submit_button_search" value="Submit" onClick="showAddress('Indonesia');"/>
               </a></li>
+			  
             <li class="search_result">
 				<?php
 					if(isset($_POST['search_input']))
@@ -77,50 +82,40 @@ if (/*@cc_on!@*/false) {
 						include_once('connect.inc.php');
 					
 						$pestId = $_POST['search_input'];
-						
-						/*$query = "SELECT latlng FROM sighting WHERE `pestId`=$_POST[search_input] AND  `reportDate` > CURDATE( ) -7";
-							
-						$result = mysqli_query($link,$query);
-						$row = mysqli_fetch_assoc($result);
-						$latlng = $row['latlng'];*/					
-						
+
 						//select any information from pest table in database
 						$sql = "SELECT * FROM sighting WHERE `pestId`= $_POST[search_input] AND  `reportDate` > CURDATE( ) -7";
 						//create a viarable to store the result
 						$pestData = mysqli_query($link,$sql);
 						echo "
 						
-						<table  width=\"20\" border=1><div class=\"cell\">
+						<table  width=\"30\" border=1><div class=\"cell\">
 						<tr>
 						<th>Sighting ID</th>
 						<th>Pest ID</th>
 						<th>Park ID</th>
-						<th>Number of Pest Sighted</th>
+						<th>Sum of Pests</th>
 						<th>Sighting Time </th>
 						<th>Latitude, Longitude</th>
 						<th>Pest Markings</th>
 						</tr>";
-
 							//update pest information
 							while($record = mysqli_fetch_array($pestData))
 							{	
 								echo "<tr>";
-								echo "<td>" . "<input type=text size=3 name=sightingId value=" . $record['sightingId'] . " style=border:0px readonly></td>";
+								echo "<td>" . "<input type=text size=7 name=sightingId value=" . $record['sightingId'] . " style=border:0px readonly></td>";
 								echo "<td>" . "<input type=text size=4 name=pestId value=" . $record['pestId'] . " style=border:0px></td>";
 								echo "<td>" . "<input type=text size=4 name=parkId value=" . $record['parkId'] . " style=border:0px></td>";
-								echo "<td>" . "<input type=text size=17 name=numberOfPestsSighted value=\"" . $record['numberOfPestsSighted'] . "\" style=border:0px></td>";
-								echo "<td>" . "<input type=text size=15 name=time value=" . $record['time'] . " style=border:0px></td>";
+								echo "<td>" . "<input type=text size=10 name=numberOfPestsSighted value=\"" . $record['numberOfPestsSighted'] . "\" style=border:0px></td>";
+								echo "<td>" . "<input type=text size=10 name=time value=" . $record['time'] . " style=border:0px></td>";
 								echo "<td>" . "<input type=text name=latlng value=\"" . $record['latlng'] . "\" style=border:0px></td>";
 								echo "<td>" . "<input type=text name=pestDetails value=\"" . $record['pestDetails'] . "\" style=border:0px></td>";
 								echo  "<td>" . "<input type=\"button\" value=\"GO!\" onClick=\"showAddress('". $record['latlng'] ."');\"/></td>";  
 								echo "</tr>";
-
-
 							}
 							echo "</div></table>";
 					}	
-				?>
-				<script> document.getElementById("submit_button_search").innerHTML=showAddress('Indonesia');</script>;			
+				?><br>
               <div id="map_canvas"></div>            
             </li>
           </ul>
