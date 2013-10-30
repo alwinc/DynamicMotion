e394cbd7d28c5f9baccb3c7482f9cc15a4ef61d1
victortanXXX <th527501@gmail.com>
2013-10-30 13:25:46 +1100

    Update confirmRegister.php

--- Prototype/old layout/confirmRegister.php
+++ Prototype/old layout/confirmRegister.php
@@ -56,7 +56,7 @@ if (/*@cc_on!@*/false) {
       <li id="account-wrap"><a href="myAccount.php">MyAccount</a></li>
       <li id="sighing-wrap"><a href="pestSightingIndex.php">Pest Sight</a></li>
       <li id="report-wrap"><a href="managementReport.php">Weekly Report</a></li>
-      <li id="register-wrap"><a href="pestRegister.php">Pest Register</a></li>
+      <li id="register-wrap"><a href="pestTracking.php">Pest Tracking</a></li>
     </ul>
   </div>
   <!-- end .header-warp -->
